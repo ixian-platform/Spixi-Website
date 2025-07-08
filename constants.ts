a7dd2dd87bea97c3fe293f919fcbfaede7c97397
IxiAngel <33629337+IxiAngel@users.noreply.github.com>
2025-07-08 18:25:31 +0200

    Spixi v0.9.8 Version Bump

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 
 
-export const spixiVersion = "v0.9.7"
+export const spixiVersion = "v0.9.8"
--- pages/download.vue
+++ pages/download.vue
@@ -15,8 +15,8 @@ useHead({
 })
 
 const downloads = {
-    'android': 'https://github.com/ixian-platform/Spixi/releases/download/v0.9.7/Spixi-v0.9.7-Android.apk',
-    'windows': 'https://github.com/ixian-platform/Spixi/releases/download/v0.9.7/Spixi-v0.9.7-Win.zip'
+    'android': 'https://github.com/ixian-platform/Spixi/releases/download/v0.9.8/Spixi-v0.9.8-Android.apk',
+    'windows': 'https://github.com/ixian-platform/Spixi/releases/download/v0.9.8/Spixi-v0.9.8-Win.zip'
 }
 </script>
 
