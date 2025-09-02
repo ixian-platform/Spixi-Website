9f2d5436057399c0a1dcbbf4eb8cc030c7df46b5
jakapozun <jaka.pozun2@gmail.com>
2025-09-02 21:12:44 +0200

    added spixi mini apps page, mini apps cards, app modal, security header added in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,104 +1,112 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-
-  modules: [
-    "@nuxtjs/i18n",
-    "@nuxtjs/color-mode",
-    "@nuxt/image",
-    "@nuxtjs/google-fonts",
-    "@nuxtjs/device",
-    "nuxt-icon",
-  ],
-
-  i18n: {
-    locales: [
-      {
-        code: 'en',
-        file: 'en.json'
-      },
-      {
-        code: 'es',
-        file: 'es.json'
-      },
-      {
-        code: 'pt',
-        file: 'pt.json'
-      },
-      {
-        code: 'fr',
-        file: 'fr.json'
-      },
-      {
-        code: 'it',
-        file: 'it.json'
-      },
-      {
-        code: 'de',
-        file: 'de.json'
-      },
-      {
-        code: 'ru',
-        file: 'ru.json'
-      },
-      {
-        code: 'ja',
-        file: 'ja.json'
-      },
-      {
-        code: 'zh',
-        file: 'zh.json'
-      },
-      {
-        code: 'id',
-        file: 'id.json'
-      },
-    ], 
-    lazy: true,
-    langDir: 'lang',
-    defaultLocale: 'en',
-    strategy: 'no_prefix',
-    compilation: {
-      strictMessage: false,
-      escapeHtml: false,
+    devtools: {enabled: true},
+    nitro: {
+        routeRules: {
+            '/**': {
+                headers: {
+                    'X-Frame-Options': 'DENY'
+                }
+            }
+        }
     },
-  },
+    modules: [
+        "@nuxtjs/i18n",
+        "@nuxtjs/color-mode",
+        "@nuxt/image",
+        "@nuxtjs/google-fonts",
+        "@nuxtjs/device",
+        "nuxt-icon",
+    ],
 
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    head: {
-      title: 'Spixi',
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+    i18n: {
+        locales: [
+            {
+                code: 'en',
+                file: 'en.json'
+            },
+            {
+                code: 'es',
+                file: 'es.json'
+            },
+            {
+                code: 'pt',
+                file: 'pt.json'
+            },
+            {
+                code: 'fr',
+                file: 'fr.json'
+            },
+            {
+                code: 'it',
+                file: 'it.json'
+            },
+            {
+                code: 'de',
+                file: 'de.json'
+            },
+            {
+                code: 'ru',
+                file: 'ru.json'
+            },
+            {
+                code: 'ja',
+                file: 'ja.json'
+            },
+            {
+                code: 'zh',
+                file: 'zh.json'
+            },
+            {
+                code: 'id',
+                file: 'id.json'
+            },
+        ],
+        lazy: true,
+        langDir: 'lang',
+        defaultLocale: 'en',
+        strategy: 'no_prefix',
+        compilation: {
+            strictMessage: false,
+            escapeHtml: false,
+        },
     },
-  },
 
-  colorMode: {
-    classSuffix: '',
-    storageKey: 'color-theme',
-    preference: 'system',
-    fallback: 'light',
-  },
-
-  googleFonts: {
-    families: {
-      'Lexend': true,
-    }
-  },
-
-  css: ['~/assets/css/main.css'],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    app: {
+        pageTransition: {name: 'page', mode: 'out-in'},
+        head: {
+            title: 'Spixi',
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        },
     },
-  },
 
-  plugins: [
-    { src: '~/plugins/utils.client.js', ssr: false },
-  ],
+    colorMode: {
+        classSuffix: '',
+        storageKey: 'color-theme',
+        preference: 'system',
+        fallback: 'light',
+    },
 
-  compatibilityDate: '2025-03-14',
+    googleFonts: {
+        families: {
+            'Lexend': true,
+        }
+    },
+
+    css: ['~/assets/css/main.css'],
+
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+
+    plugins: [
+        {src: '~/plugins/utils.client.js', ssr: false},
+    ],
+
+    compatibilityDate: '2025-03-14',
 })
